c45e53712ee9d593cb385e97a643ffe486bef470
Lailton17 <lailtonsousa75@gmail.com>
2026-05-09 10:40:39 -0300

    Python Entradas

---
+++ 03_python_entradas/entradas.py
@@ -0,0 +1,10 @@
+# Autor: Lailton Sousa
+# Projeto: Estudando entradas e saídas em Python
+
+# Declaração de variáveis
+valor1 = 10
+valor2= 20
+resultado = valor1 + valor2
+
+# Exibindo resultados
+print (resultado)
---
+++ 03_python_entradas/entradas4.py
@@ -0,0 +1,17 @@
+# Autor: Lailton Sousa
+# Projeto: Entradas com nomes e diferentes operações
+
+# variáveis
+nome = input('Digite seu nome: ')
+valor1 = int(input('Digite o primeiro valor: '))
+valor2 = int(input('Digite o segundo valor: '))
+soma = valor1 + valor2 
+multiplicacao = valor1 * valor2
+subtracao = valor1 - valor2
+divisao = valor1 / valor2
+
+print(nome)
+print('A soma dos números é: ', soma)
+print('A multiplicação dos números é: ', multiplicacao)
+print('A subtração dos números é: ', subtracao)
+print('A divisão dos números é: ', divisao)
